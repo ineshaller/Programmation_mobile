1982081fcdc83bdd4ac80c47accc222d32af399f
ineshaller <ines.haller@edu.ece.fr>
2026-02-01 21:23:18 +0100

    requêtes appel réseau (bug avec fromJSON)

--- exercices/lib/product_notifier.dart
+++ exercices/lib/product_notifier.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:formation_flutter/model/product.dart';
+import 'package:dio/dio.dart';
+import 'dart:convert';
 
 class ProductNotifier extends ChangeNotifier {
   Product? _product;
   Product? get product => _product;
-  void loadProduct(){
-    _product = generateProduct();
+  Future<void> loadProduct() async{
+    Dio dio = Dio();
+    Response<dynamic> response = await dio.get('https://api.formation-flutter.fr/v2/getProduct?barcode=5000159484695');
+    Map<String, dynamic> json = jsonDecode(response.data);
+    final apiProduct = Product.fromJSON(json);
     notifyListeners();
   }
   ProductNotifier(){
